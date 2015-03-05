43fb4bd8c8137e7dcb5237bfa897a41250f513cd
Guest <vsoares@inf.ufsm.br>
2015-03-05 08:56:54 -0300

    aula1

--- AULA.c
+++ AULA.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
+
+void main(){
+	int matricula;
+	FILE*f;
+	char nome[100];
+	printf("Digite matricula:");
+	scanf("%d",&matricula);
+	printf("Nome:");
+	scanf("%s",nome);
+	f = fopen ("saida.txt","w");
+	fprintf(f,"%d%s\n",matricula,nome);
+	fclose(f);
+}
 
